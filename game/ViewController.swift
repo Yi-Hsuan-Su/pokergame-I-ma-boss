623e79ea99048a3b0dc509b111548cbe6a7cf8ee
Ethan <Ethan@hail.com>
2019-05-08 09:03:51 -0700

    Initial Commit

---
+++ game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  game
+//
+//  Created by EA on 2019/5/8.
+//  Copyright © 2019 EA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
